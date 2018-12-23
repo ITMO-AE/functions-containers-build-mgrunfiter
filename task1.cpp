9cf03d6d27d90ac84b626172ccd6d00685b92b88
Sergey Cherevko <mrgunfiter@gmail.com>
2018-12-23 20:51:43 +0300

    1st commit into branch develop

---
+++ task1.cpp
@@ -0,0 +1,44 @@
+﻿/*
+1. 
+Напишите функцию, которая:
+* называется Factorial
+ * возвращает int
+* принимает int и возвращает факториал своего аргумента.
+
+Гарантируется, что аргумент функции по модулю не превышает 10.
+Для отрицательных аргументов функция должна возвращать 1\.
+
+*/
+#include <iostream>
+
+using namespace std;
+
+int Factorial(int n)
+{
+    int f = 1;
+
+    if (n >= 0) {
+        for (int i = 2; i <= n; i++)
+            f *= i;
+    }
+
+    return f;
+
+}
+
+int Factorial1(int n)
+{
+    if (n <= 0)
+        return 1;
+    return (n * Factorial1(n - 1));
+
+}
+
+int main()
+{
+    int n = 5;
+
+    cout << " n! = " << Factorial(n) << endl;
+    cout << " n! = " << Factorial1(n) << endl;
+    return 0;
+}
---
+++ task3.cpp
@@ -0,0 +1,18 @@
+﻿/*
+3.
+Напишите функцию, которая
+* называется PalindromFilter
+* возвращает vector<string> 
+* принимает vector<string> words и int minLength 
+* возвращает все строки из вектора words, которые являются палиндромами и имеют длину не меньше minLength
+
+Входной вектор содержит не более 100 строк, длина каждой строки не больше 100 символов.
+
+Пример:
+
+| words | minLength |	результат|
+|---|---|---|
+|abacaba, aba	|5|	abacaba |
+|abacaba, aba	|2|	abacaba, aba |
+|weew, bro, code |4|	weew |
+*/
